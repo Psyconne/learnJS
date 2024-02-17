0d8c57a15ab3fcd4b838bfb6bb25c6181dec50a7
psyconne <elidrissi.imen@gmail.com>
2024-02-18 00:02:45 +0100

    exercise p:122 Eloquent JS range & sum functions part2

--- sumOfRange.js
+++ sumOfRange.js
@@ -1,18 +1,20 @@
 function range(start, end, step=1) {
     //return [start, start+step, ..,end-step, end]
     let result = [];
-    for (let i=start; i<=end; i=i+step) {
-	result.push(i);
+    if (start < end) {
+	for (let i=start; i<=end; i=i+step) result.push(i);	
+   } else { 
+       	for (let i=end; i<=start; i=i-step) result.unshift(i);
     }
-    return result;
+	return result;
 }
 
-function sum(arrayOfNum) {
+function sum(array) {
     let result = 0;
-    for (let i=0; i<arrayOfNum.length; i++) {
-	result += arrayOfNum[i];
+    for (let value of array) {
+	result += value;
     }
     return result;
 }
 
-console.log(sum(range(1, 10, 2)));
+console.log(sum(range(5, 10, 1)));
